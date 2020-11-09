b35aaffa4f9b76863a33c52f8a9a8b6c7632f760
Chandler R <chandler.renteria@gmail.com>
2020-11-08 17:12:04 -0700

    finished home page with review slider and contact form, several spacing and formatting issues remain

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ for (let x = 0; x < navTabs.length; x++){
         navTabs[x].classList.add("active");
     })
 }
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("carouselslide");
+  showIndex = slideIndex -1
+  if (n > 2) {slideIndex = 0}
+  if (n < 0) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[showIndex].style.display = "flex";  
+}
